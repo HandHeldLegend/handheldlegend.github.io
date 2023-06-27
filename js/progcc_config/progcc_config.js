6109f0cfba5479e10053c396ba5c205fbd5c0cf3
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-06-26 17:22:34 -0700

    early progcc cfg website

---
+++ js/progcc_config/progcc_config.js
@@ -0,0 +1,44 @@
+
+const LED_GROUP_LEFTSTICK   = 0;
+const LED_GROUP_RIGHTSTICK  = 1;
+const LED_GROUP_A           = 2;
+const LED_GROUP_B           = 3;
+const LED_GROUP_X           = 4;
+const LED_GROUP_Y           = 5;
+const LED_GROUP_D           = 6;
+const LED_GROUP_M           = 7;
+
+// RGB LED functions
+function set_rgb_brightness(brightness)
+{
+
+}
+
+
+function set_rgb_color(led_group, color)
+{
+
+}
+
+
+// Analog stick calibration
+function start_analog_calibration()
+{
+
+}
+
+function stop_analog_calibration()
+{
+
+}
+
+function save_analog_calibration()
+{
+
+}
+
+// Snapback viewer functions
+function get_snapback_axis(axis)
+{
+
+}
