71b3cb1be8703b704d5ac322918e89614bc44a0a
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-07-25 10:24:53 -0700

    test2

--- js/web_test/web_test.js
+++ js/web_test/web_test.js
@@ -4,10 +4,16 @@ let device;
 const connectButton = document.getElementById('connectButton');
 async function clickButton()
 {
+  addEventListener("connect", (event) => {
+    console.log("Device connected.");
+  });
+
   // Request permission to access the gamepad
   device = await navigator.usb.requestDevice({ filters: [{ vendorId: 0x057E, productId: 0x2009 }] });
 
   await device.open();
-  await device.selectConfiguration(1);
   await device.claimInterface(0);
+
+  console.log(device.productName);
+  console.log(device.manufacturerName);
 };
