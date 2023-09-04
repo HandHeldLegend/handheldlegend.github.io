4d844b996d52e9a832542ff15f2f6e0332af424a
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-09-04 00:06:03 -0700

    New WebUSB timeout firmware for ProGCC

--- js/progcc_config/progcc_config.js
+++ js/progcc_config/progcc_config.js
@@ -6,7 +6,7 @@ let menuToggles = document.getElementsByClassName("toggle");
 let menuToggleLabels = document.getElementsByClassName("lbl-toggle");
 let selectedToggle = null;
 
-const FIRMWARE_VERSION = 0x0A08;
+const FIRMWARE_VERSION = 0x0A09;
 
 const WEBUSB_CMD_FW_SET = 0x0F;
 const WEBUSB_CMD_FW_GET = 0xAF;
