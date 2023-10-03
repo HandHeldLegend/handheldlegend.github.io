ed433396c34f013ba5cbe02bea446b9b1ec147c5
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-10-02 17:18:34 -0700

    R1 update

--- js/progcc_config/progcc_version.js
+++ js/progcc_config/progcc_version.js
@@ -1,14 +1,15 @@
 const CONFIG_DEVICES = 
 {
-    0xA001 : 0x0A13, // ProGCC 3
+    0xA001 : 0x0A14, // ProGCC 3
 };
 
 const FW_CHANGELOG = 
 {
     0xA001 : // ProGCC 3
-    `10.2.2023 Update:<br>
+    `10.2.2023r1 Update:<br>
     - Adds dual Z map option<br>
     - Adjust rumble sensitivity parameters<br>
+    - Test fix for rumble disabling mid-play<br>
     `
 }
 
