de8f94f4cb5d6e48488ab046f5190e5dc06d57b5
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-12-17 01:20:47 -0800

    Update container display

--- hoja_config/hoja_sw.js
+++ hoja_config/hoja_sw.js
@@ -1,6 +1,6 @@
 // SW Updated Date: 12/16/2023
 
-const CACHE_NAME = 'hoja-pwa-cache-v16';
+const CACHE_NAME = 'hoja-pwa-cache-v17';
 
 const root_css_url = 'https://handheldlegend.github.io/css/'
 
