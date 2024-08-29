461b1cfa40cb619adcc3ebb3af137a93610598a6
Mitch Cairns <mitch.cairns@handheldlegend.com>
2024-08-29 13:40:18 -0700

    Update Device IDs

--- hoja_config/hoja_sw.js
+++ hoja_config/hoja_sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v3';
 const CACHE_NAME = `hoja-site-cache-${CACHE_VERSION}`;
 
 const root_css_url = 'https://handheldlegend.github.io/css/'
--- hoja_config/js/modules/device_ids.js
+++ hoja_config/js/modules/device_ids.js
@@ -40,12 +40,15 @@ const DEVICE_FW_MANIFEST_URLS =
     0xA001  : "https://raw.githubusercontent.com/HandHeldLegend/ProGCC-V3-RP2040/main/PROGCC_RP2040_manifest.json",
     0xA002  : "https://raw.githubusercontent.com/HandHeldLegend/ProGCC-V3-Plus/master/FW/PROGCCPLUS_ERM_RP2040_manifest.json", // ProGCC 3+
     0xA004  : "https://raw.githubusercontent.com/HandHeldLegend/ProGCC-V3-Plus/master/FW/PROGCC3.1_RP2040_manifest.json", // ProGCC 3.1
+    0xA005  : "https://raw.githubusercontent.com/HandHeldLegend/ProGCC-V3-Plus/master/FW/PROGCC3.1_RP2040_manifest.json", // ProGCC 3.2
 
     0xB001  : "https://raw.githubusercontent.com/HandHeldLegend/SuperGamepadPlus/master/FW/SUPERGAMEPAD_RP2040_manifest.json",  // SuperGamepad+
     0xB002  : "https://raw.githubusercontent.com/HandHeldLegend/UniSNES/master/FW/UNISNES_RP2040_manifest.json",           // UniSNES
 
     0xC001  : "https://raw.githubusercontent.com/HandHeldLegend/GC-Ultimate/master/FW/GCULTIMATE_RP2040_manifest.json",       // GC Ultimate PROTOTYPE
     0xC002  : "https://raw.githubusercontent.com/HandHeldLegend/GC-Ultimate/master/FW/GCULTIMATE_R4_RP2040_manifest.json",       // GC Ultimate R4
+    0xC003  : "https://raw.githubusercontent.com/HandHeldLegend/GC-Ultimate/master/FW/GCULTIMATE_R4_RP2040_manifest.json",       // GC Ultimate R4K
+
     0xA00A  : "https://raw.githubusercontent.com/mitchellcairns/HOJA-ThingamaPro/master/THINGAMAPRO_RP2040_manifest.json", // ThingamaPro (Esca)
 
     0xD001  : "https://raw.githubusercontent.com/HandHeldLegend/HOJA-Fightingbox-Mini/main/manifest.json", // Fightingbox mini (https://github.com/Fightingbox/Fightingbox-mini)
@@ -58,11 +61,14 @@ const FW_UPDATE_URLS =
     0xA001 : "https://raw.githubusercontent.com/HandHeldLegend/ProGCC-V3-RP2040/main/build/PROGCC_RP2040",                 // ProGCC 3
     0xA002 : "https://raw.githubusercontent.com/HandHeldLegend/ProGCC-V3-Plus/master/FW/build/PROGCCPLUS_ERM_RP2040",      // ProGCC 3+
     0xA004 : "https://raw.githubusercontent.com/HandHeldLegend/ProGCC-V3-Plus/master/FW/build/PROGCC3.1_RP2040",           // ProGCC 3.1
+    0xA005  : "https://raw.githubusercontent.com/HandHeldLegend/ProGCC-V3-Plus/master/FW/build/PROGCC3.2_RP2040",          // ProGCC 3.2
 
     0xB001 : "https://raw.githubusercontent.com/mitchellcairns/SuperGamepadPlus/master/FW/build/SUPERGAMEPAD_RP2040",      // SuperGamepad+
     0xB002 : "https://raw.githubusercontent.com/HandHeldLegend/UniSNES/master/FW/build/UNISNES_RP2040",                    // UniSNES
     0xC001  : "https://raw.githubusercontent.com/HandHeldLegend/GC-Ultimate/master/FW/build/GCULTIMATE_RP2040", // GC Ultimate
     0xC002  : "https://raw.githubusercontent.com/HandHeldLegend/GC-Ultimate/master/FW/build/GCULTIMATE_R4_RP2040", // GC Ultimate R4,
+    0xC003  : "https://raw.githubusercontent.com/HandHeldLegend/GC-Ultimate/master/FW/build/GCULTIMATE_R4K_RP2040", // GC Ultimate R4K,
+
     0xA00A : "https://raw.githubusercontent.com/mitchellcairns/HOJA-ThingamaPro/master/build/THINGAMAPRO_RP2040",          // ThingamaPro
 
     0xF001  : "https://raw.githubusercontent.com/HandHeldLegend/HOJA-PHOB-2.1/main/build/HOJA-PHOB"             // Phob 2.1
