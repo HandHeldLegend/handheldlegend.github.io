40d6798166a383fd5b927bc97456e63285786f81
Mitch Cairns <mitch.cairns@handheldlegend.com>
2025-01-24 14:09:11 -0800

    Display battery info

--- hoja2/modules/battery-md.js
+++ hoja2/modules/battery-md.js
@@ -1,4 +1,5 @@
-// Import the number selector (optional, as it's now globally defined)
+import HojaGamepad from '../js/gamepad.js';
+
 import NumberSelector from '../components/number-selector.js';
 import MultiPositionButton from '../components/multi-position-button.js';
 import GroupRgbPicker from '../components/group-rgb-picker.js';
@@ -10,75 +11,25 @@ import SingleShotButton from '../components/single-shot-button.js';
 
 import { enableTooltips } from '../js/tooltips.js';
 
+const parseBatteryBufferText = buffer => {
+    const text = new TextDecoder().decode(buffer).trim();
+    // Log each character code to see what's actually in there
+    //console.log('Character codes:', [...text].map(c => c.charCodeAt(0)));
+    
+    // Remove any null bytes or other whitespace and then check
+    const cleanText = text.replace(/\0+/g, '').trim();
+    return cleanText === '~' ? false : cleanText;
+};
+
+/** @type {HojaGamepad} */
+const gamepad = HojaGamepad.getInstance();
+
 export function render(container) {
     container.innerHTML = `
-            <number-selector 
-                label="Brightness" 
-                type="integer" 
-                min="0" 
-                max="100" 
-                step="5" 
-                default-value="50"
-            ></number-selector>
-
-            <h2>Brightness</h2>
-    
-            <number-selector 
-                label="Brightness" 
-                type="integer" 
-                min="0" 
-                max="100" 
-                step="5" 
-                default-value="50"
-            ></number-selector>
-
-            <h2>Mode</h2>
-            <multi-position-button 
-                id="rgbModeButton" 
-                labels="User, Rainbow"
-                default-selected="0"
-            ></multi-position-button>
-
-            <h2>Colors</h2>
-            <group-rgb-picker 
-                group-name="A" 
-                color="22AA22"
-            ></group-rgb-picker>
-
-            <group-rgb-picker 
-                group-name="B" 
-                color="22AA22"
-            ></group-rgb-picker>
-
-            <group-rgb-picker 
-                group-name="X" 
-                color="22AA22"
-            ></group-rgb-picker>
-
-            <angle-selector 
-                in-angle="45"
-                out-angle="45"
-                distance="2048"
-            ></angle-selector>
-
-            <remap-selector
-                in-value="A",
-                out-value="B",
-            ></remap-selector>
-
-            <tristate-button 
-                id="component-test-tristate"
-                off-text="Connect" 
-                on-text="Disconnect" 
-                off-to-on-transitioning-text="Connecting..." 
-                on-to-off-transitioning-text="Disconnecting..."
-            ></tristate-button>
-
-            <single-shot-button 
-                id="component-test-singleshot" 
-                state="ready"
-                ready-text="Save" 
-                pending-text="Saving..."
-            ></single-shot-button>
+    <h2>Battery Information</h2>
+    <h3>Battery Model</h3>
+    <div class="app-text-container">
+        ${parseBatteryBufferText(gamepad.battery_static.part_number)}
+    </div>
     `;
 }
