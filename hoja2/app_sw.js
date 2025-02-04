16096f0696a4f026baebecf9eafc5feaf59b20ba
Mitch Cairns <mitch.cairns@handheldlegend.com>
2025-02-03 20:34:18 -0800

    force update

--- hoja2/app_sw.js
+++ hoja2/app_sw.js
@@ -1,5 +1,5 @@
 const CACHE_CONFIG = {
-    version: 'v0.13.0', // Increment this when you update files
+    version: 'v0.13.1', // Increment this when you update files
     folders: {
         '/': ['index.html', 'attributions.txt'],
         '/js/': ['app.js', 'module-registry.js', 'gamepad.js', 'tooltips.js', 'legacy.js'],
