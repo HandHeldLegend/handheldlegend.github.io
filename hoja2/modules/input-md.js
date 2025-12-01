ae1a1e8f0e08bacaf5f9497fdfd0fe057c85119c
Mitch Cairns <mitchellcairns@gmail.com>
2025-12-01 13:01:34 -0800

    checkpoint

--- hoja2/modules/input-md.js
+++ hoja2/modules/input-md.js
@@ -5,16 +5,36 @@ import InputConfigPanel from '../components/input-config-panel.js';
 import ButtonGrid from '../components/button-grid.js';
 
 import { enableTooltips } from '../js/tooltips.js';
+import Inputinfoslot from '../factory/parsers/inputInfoSlot.js';
 
 /** @type {HojaGamepad} */
 const gamepad = HojaGamepad.getInstance();
 const gamepadCfgBlockNumber = 0;
+
+/** @type {InputMappingDisplay[]} */
+let inputMappingDisplays = [];
 
 let blurElement = null;
 let configPanelElement = null;
 let configPanelComponent = null;
 let remapPanelElement = null;
 let buttonGridComponent = null;
+let currentRemapProfileIndex = 0;
+let inputInfoList = [];
+
+function inputReportHook(data) {
+    for (let i = 16; i < (16 + 36); i++) {
+        let idxOutput = i - 16;
+        let inputType = inputInfoList[idxOutput].type;
+        let mappingIdx = inputInfoList[idxOutput].mappingIndex;
+
+        if(inputType != 0) {
+
+            let inputValue = data.getUint8(i);
+            inputMappingDisplays[mappingIdx].setValue(inputValue);
+        }
+    }
+}
 
 async function writeGamepadMemBlock() {
     await gamepad.sendBlock(gamepadCfgBlockNumber);
@@ -25,40 +45,44 @@ function stringToArray(input) {
     return input.split(',');
 }
 
+function decodeText(buffer) {
+    const decoder = new TextDecoder('utf-8');
+    const str = decoder.decode(buffer);
 
+    // Remove any null characters (0x00) from the string
+    return str.replace(/\x00/g, '');
+}
 
 function closeOverlays() {
-    if(!remapPanelElement) return;
+    if (!remapPanelElement) return;
     remapPanelElement.classList.add('hidden');
 
-    if(!configPanelElement) return;
+    if (!configPanelElement) return;
     configPanelElement.classList.add('hidden');
 
-    if(!blurElement) return;
+    if (!blurElement) return;
     blurElement.classList.add('hidden');
 }
 
 function openRemapPanel() {
 
-    if(!blurElement) return;
+    if (!blurElement) return;
     blurElement.classList.remove('hidden');
 
-    if(!remapPanelElement) return;
+    if (!remapPanelElement) return;
     remapPanelElement.classList.remove('hidden');
-
-    
 }
 
 function remapClickHandler(event) {
     console.log("Remap clicked:", event);
-    switch(event.index) {
+    switch (event.index) {
         case -2: // Cancel
             closeRemapPanel();
             return;
         case -1: // Unmap
             configPanelComponent.setOutputLabelAndType('Disabled', 'none');
             closeRemapPanel();
-            break;  
+            break;
 
         default:
             configPanelComponent.setOutputLabelAndType(event.label, 'analog');
@@ -68,20 +92,20 @@ function remapClickHandler(event) {
 }
 
 function closeRemapPanel() {
-    if(!remapPanelElement) return;
+    if (!remapPanelElement) return;
     remapPanelElement.classList.add('hidden');
 }
 
-function inputMapClicked(event) { 
+function inputMapClicked(event) {
     console.log("Input mapping clicked:", event.inputLabel);
 
     configPanelComponent.setInputLabelAndType(event.inputLabel, 'analog');
     configPanelComponent.setOutputLabelAndType(event.outputLabel, 'analog');
 
-    if(!configPanelElement) return;
+    if (!configPanelElement) return;
     configPanelElement.classList.remove('hidden');
 
-    if(!blurElement) return;
+    if (!blurElement) return;
     blurElement.classList.remove('hidden');
 }
 
@@ -91,10 +115,39 @@ const inputList = [
 
 export function render(container) {
 
+    let moduleGridHTML = '';
+    let mappingDisplayIndex = 0;
+    let mappingIndex = 0;
+
+    // Clear inputInfoList
+    inputInfoList = [];
+
+    gamepad.input_static.input_info.forEach((inputInfo, index) => {
+
+        let inputName = decodeText(inputInfo.input_name);
+        
+        let inputType = inputInfo.input_type;
+
+        if(inputType != 0) 
+        {
+            mappingIndex = mappingDisplayIndex;
+            mappingDisplayIndex++;
+            moduleGridHTML += `<input-mapping-display input-label="${inputName}" output-label="Unmapped" value="0" pressed="false"></input-mapping-display>`;
+        }
+
+        // Push info to list
+        inputInfoList.push({
+            mappingIndex: mappingIndex,
+            name: inputName,
+            type: inputType
+        });
+    });
+
+    console.log(inputInfoList)  ;
+
     container.innerHTML = `
             <h2>Input Mode</h2>
             
-
             <div id="panel_blur" class="popup-blur hidden" style="z-index: 199;"></div>
 
             <div id="config_panel" class="popup hidden" style="z-index: 200;">
@@ -107,54 +160,7 @@ export function render(container) {
 
             <h2>Input Setup</h2>
             <div class="module-grid">
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="false"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
-            <input-mapping-display input-label="B" output-label="X" value="200" pressed="true"></input-mapping-display>
+            ${moduleGridHTML}
             </div>
     `;
 
@@ -167,19 +173,21 @@ export function render(container) {
 
     blurElement = container.querySelector('#panel_blur');
 
-    const inputMappings = container.querySelectorAll('input-mapping-display');
-    inputMappings.forEach((mapping) => {
+    inputMappingDisplays = container.querySelectorAll('input-mapping-display');
+    inputMappingDisplays.forEach((mapping) => {
         mapping._onClick = inputMapClicked;
     });
+
+    console.log(inputMappingDisplays);
 
     configPanelElement = container.querySelector('#config_panel');
 
     configPanelComponent._onClose = closeOverlays;
-    
+
     blurElement.addEventListener('click', (e) => {
-        if(e.target === blurElement) {
+        if (e.target === blurElement) {
             closeOverlays();
-        }   
+        }
     });
 
     configPanelComponent.setOutputSelectorHandler(() => {
@@ -188,4 +196,7 @@ export function render(container) {
 
     // Populate the button grid with input labels
     buttonGridComponent.setButtons(inputList);
+
+    // Set report hook
+    gamepad.setReportHook(inputReportHook);
 }
